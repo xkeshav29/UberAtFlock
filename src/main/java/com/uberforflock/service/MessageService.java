ff0a160e4aad198446faa0973b2e12a5097cb35b
devesh <ur.devesh@gmail.com>
2016-08-23 18:07:04 +0530

    added something

--- src/main/java/com/uberforflock/service/MessageService.java
+++ src/main/java/com/uberforflock/service/MessageService.java
@@ -1,7 +1,12 @@
 package com.uberforflock.service;
 
+import co.flock.www.FlockApiClient;
 import co.flock.www.model.flockevents.SlashCommand;
+import co.flock.www.model.messages.FlockMessage;
+import co.flock.www.model.messages.Message;
+import com.uberforflock.dao.UserTokenDao;
 import com.uberforflock.model.Availability;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,7 +14,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MessageService {
-    public void sendAvailabilityMessage(Availability availability, SlashCommand slashCommand){
-
+    @Autowired
+    private UserTokenDao userTokenDao;
+    public void sendAvailabilityMessage(Availability availability, SlashCommand slashCommand) throws  Exception{
+        String userToken = userTokenDao.getUserToken(slashCommand.getUserId());
+        FlockApiClient flockApiClient = new FlockApiClient(userToken,false);
+        Message message = new Message(slashCommand.getChat(),availability.getTimes().size() > 0 ? "We have uber for you" : "Sorry no uber now");
+        message.setAppId("d21753b9-c55b-4514-88a5-5c199c1b7801");
+        String id = flockApiClient.chatSendMessage(new FlockMessage(message));
     }
 }
