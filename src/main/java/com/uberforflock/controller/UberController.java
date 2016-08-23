b0e15456aea6cdd5a67610bd3ed89420a32d221b
k-keshav <kumar@riva.co>
2016-08-23 17:05:57 +0530

    controller

--- src/main/java/com/uberforflock/controller/UberController.java
+++ src/main/java/com/uberforflock/controller/UberController.java
@@ -1,11 +1,13 @@
 package com.uberforflock.controller;
 
 
+import co.flock.www.FlockEventsHandlerClient;
 import co.flock.www.model.flockevents.SlashCommand;
 import com.google.gson.Gson;
 import com.uberforflock.dao.OauthCredentialDao;
 import com.uberforflock.model.Availability;
 import com.uberforflock.model.OauthCredential;
+import com.uberforflock.model.UberFlockEventHandler;
 import com.uberforflock.service.CsrfTokenService;
 import com.uberforflock.service.MessageService;
 import com.uberforflock.service.Oauth2TokenService;
@@ -13,7 +15,6 @@ import com.uberforflock.service.UserAuthService;
 import com.uberforflock.util.Constants;
 import com.uberforflock.util.Util;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
@@ -31,8 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -65,13 +66,17 @@ public class UberController {
     @Autowired
     private UserAuthService userAuthService;
 
+    @Autowired
+    private UberFlockEventHandler eventHandler;
+
     @RequestMapping(value = "/authUrl", method = RequestMethod.GET)
     public String getAuthUrl(@RequestParam String userid){
         try {
             OAuthClientRequest.AuthenticationRequestBuilder requestBuilder = OAuthClientRequest
                     .authorizationLocation("https://login.uber.com/oauth/v2/authorize")
                     .setClientId("oZz5Gb_QSSe58a58C1hak9pURrnoYiU6")
-                    .setRedirectURI("http://localhost:7011/callback")
+                    .setRedirectURI(Constants.OAUTH_CALLBACK_URL)
+                    .setScope("request profile")
                     .setState(getState(userid));
             Map<String, String> customParams = Collections.singletonMap("response_type","code");
             if (customParams != null && !customParams.isEmpty())
@@ -85,7 +90,7 @@ public class UberController {
     }
 
     @RequestMapping(value = "/callback", method = RequestMethod.GET)
-    public void callbackHandler(@RequestParam Map<String, String> params, HttpServletResponse response){
+    public String callbackHandler(@RequestParam Map<String, String> params, HttpServletResponse response){
         List<NameValuePair> qparams = new ArrayList<>();
         try {
             String code = params.get("code");
@@ -97,35 +102,22 @@ public class UberController {
             qparams.add(new BasicNameValuePair("flockValidationToken", flockValidationToken));
             if (code == null || code.isEmpty()) {
                 logger.info("Auth code not found for user {}", user);
-                qparams.add(new BasicNameValuePair("status", "failure"));
-                Util.redirect(response, Constants.FLOCK_REDIRECT_URL, qparams);
-                return;
+                return "failed";
             }
 
             if (!csrfTokenService.isValidToken(csrf_token, user)) {
                 logger.info("Invalid csrf Token " + csrf_token + " for the user " + user);
-                qparams.add(new BasicNameValuePair("status", "failure"));
-                Util.redirect(response, Constants.FLOCK_REDIRECT_URL, qparams);
-                return;
+                return "failed";
             }
             OauthCredential credential = oauthtokenService.getOauth2Credential( code, GrantType.AUTHORIZATION_CODE);
             credential.setDisplayName("dummy");
             oauthCredentialDao.setCredential(stateMap.get("userid"), credential);
             logger.info("Saved credential for user {} for app {}", user);
             logger.info("Redirecting to:" + Constants.FLOCK_REDIRECT_URL);
-            String uriParams = URLEncodedUtils.format(qparams, "UTF-8");
-            String redirectUri = Constants.FLOCK_REDIRECT_URL + "?" + uriParams;
-            response.sendRedirect(redirectUri);
+            return "User Authenticated";
         } catch (Exception e) {
             logger.error("Authentication failed", e);
-            qparams.add(new BasicNameValuePair("status", "failure"));
-            String uriParams = URLEncodedUtils.format(qparams, "UTF-8");
-            String redirectUri = Constants.FLOCK_REDIRECT_URL + "?" + uriParams;
-            try {
-                response.sendRedirect(redirectUri);
-            } catch (IOException ioe) {
-                logger.error("Error redirecting to:{}", redirectUri);
-            }
+            return "failed";
         }
     }
 
@@ -134,11 +126,6 @@ public class UberController {
         state.put("userid", userId);
         state.put("csrf_token", csrfTokenService.getCsrfToken(userId));
         return new Gson().toJson(state);
-    }
-
-    @RequestMapping(value = "/oauthcomplete", method = RequestMethod.GET)
-    public void oauthComplete(@RequestParam String user){
-        logger.info("Oauth completed for user:", user);
     }
 
     @RequestMapping(value = "/availability", params = {"lon", "lat", "flockEvent"}, method = RequestMethod.GET)
@@ -160,24 +147,6 @@ public class UberController {
         }
     }
 
-    @RequestMapping(value = "/availabilitytest", params = {"lon", "lat"}, method = RequestMethod.GET)
-    public void getAvailabilityTest(@RequestParam String lon, @RequestParam String lat){
-        try {
-            String url = "https://api.uber.com/v1/estimates/time?start_latitude=" + lat + "&start_longitude=" + lon;
-            logger.info("Getting availability for long {} and lat {}", lon, lat);
-            HttpHeaders headers = new HttpHeaders();
-            //  headers.set("Authorization", "Token " + Constants.SERVER_TOKEN);
-            String accessToken = userAuthService.getAccessToken("kk");
-            headers.set("Authorization", "Token " + accessToken);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            Availability availability = new Gson().fromJson(response.getBody(), Availability.class);
-            logger.info(response.getBody());
-        }catch(Exception e){
-            logger.error("Exception getting availability for flockEvent", e);
-        }
-    }
-
     @RequestMapping(value = "/doaouth")
     public ModelAndView doaouth(@RequestParam String flockValidationToken){
         try {
@@ -195,19 +164,30 @@ public class UberController {
         return "alive";
     }
 
-    @RequestMapping(value = "/ride", params = {"product_id, start_latitude, start_longitude"}, method = RequestMethod.POST)
+    @RequestMapping(value = "/ride", method = RequestMethod.POST)
     public void ride(@RequestParam String product_id, @RequestParam String start_latitude, @RequestParam String start_longitude){
-        String url = "https://sandbox-api.uber.com/v1/requests";
-        Map<String,String> params = new HashMap<>();
-        params.put("product_id", product_id );
-        params.put("start_latitude", start_latitude);
-        params.put("start_longitide", start_longitude);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Token " + Constants.SERVER_TOKEN);
-        //HttpEntity<String> httpEntity = new HttpEntity<String>(params, headers);
-       // ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity , String.class);
-        //restTemplate.exchange(url, RequestMethod.POST, httpEntity, String.class, null);
-        //return "";
+        try {
+            String url = "https://sandbox-api.uber.com/v1/requests";
+            HttpHeaders headers = new HttpHeaders();
+            Map<String, Object> postpayload = new HashMap<>();
+            postpayload.put("product_id", product_id);
+            postpayload.put("start_latitude", start_latitude);
+            postpayload.put("start_longitide", start_longitude);
+            String accessToken = userAuthService.getAccessToken("kk");
+            headers.set("Authorization", "Bearer " + accessToken);
+            HttpEntity<String> httpEntity = new HttpEntity<>(new Gson().toJson(postpayload), headers);
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class, Collections.emptyMap());
+            logger.info(response.getBody());
+        }catch (Exception e) {
+            logger.error("Error requesting ride", e);
+        }
+    }
+
+    @RequestMapping(value = "/flockeventcallback", method = RequestMethod.POST)
+    public void flockEventCallbackHandler(HttpServletRequest request) throws Exception{
+        FlockEventsHandlerClient eventsHandlerClient = new FlockEventsHandlerClient(eventHandler, Constants.APP_SECRET);
+        eventsHandlerClient.Handle(request);
+        logger.info("Recieved flock event");
     }
 
 }
