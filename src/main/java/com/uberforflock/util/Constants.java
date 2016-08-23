6810fe52d9664d8592b8a6dc63ec9c79326eec4b
k-keshav <kumar@riva.co>
2016-08-23 17:09:48 +0530

    add constants

--- src/main/java/com/uberforflock/util/Constants.java
+++ src/main/java/com/uberforflock/util/Constants.java
@@ -8,11 +8,13 @@ public class Constants {
 
     public static final String CLIENT_SECRET="EB5PONgMabrbBrAhlc7ldzW4UXA7zE72sdutSCXQ";
 
-    public static final String FLOCK_REDIRECT_URL = "http://localhost:7011/oauthcomplete";
+    public static final String FLOCK_REDIRECT_URL = "http://localhost:7012/oauthcomplete";
 
-    public static final String OAUTH_CALLBACK_URL = "http://localhost:7011/callback";
+    public static final String OAUTH_CALLBACK_URL = "http://localhost:7012/callback";
 
     public static final String TOKEN_URL = "https://login.uber.com/oauth/v2/token";
 
     public static final String SERVER_TOKEN = "NYHCnf5R8rI6JnU71bdE0_lEJ7KyH5bsgDAUZKqu";
+
+    public static final String APP_SECRET = "bd6928c7-9439-457b-b617-ab4e3fee0b30";
 }
